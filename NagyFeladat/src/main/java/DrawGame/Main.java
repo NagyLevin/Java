31a5fb7afba95810f5e31190a432c26b0ad93a64
Levin <levsza13@gmail.com>
2024-04-17 22:34:56 +0200

    nagyfeladat elkezdve

---
+++ NagyFeladat/src/main/java/DrawGame/Main.java
@@ -0,0 +1,17 @@
+package DrawGame;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        ReadPromts RP = new ReadPromts();
+        RP.readfile("promts.txt");
+        RP.readpromts();
+
+
+
+
+    }
+
+
+}
